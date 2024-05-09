f6ecc665f2f9a548d8d7e01c2e0381ca76779b91
maninbule <2373926690@qq.com>
2024-05-09 14:36:39 +0800

    添加了测试卷36

---
+++ 测试卷/测试卷36/03-3019. 按键变更的次数.py
@@ -0,0 +1,7 @@
+'''
+https://leetcode.cn/problems/number-of-changing-keys/description/
+'''
+
+class Solution:
+    def countKeyChanges(self, s: str) -> int:
+        pass
---
+++ 测试卷/测试卷36/04-3079. 求出加密整数的和.py
@@ -0,0 +1,9 @@
+'''
+https://leetcode.cn/problems/find-the-sum-of-encrypted-integers/description/
+'''
+from typing import List
+
+
+class Solution:
+    def sumOfEncryptedInt(self, nums: List[int]) -> int:
+        pass
