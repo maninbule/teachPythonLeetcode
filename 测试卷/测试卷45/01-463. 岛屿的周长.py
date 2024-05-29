2354120484e534e30c6c817d1a87f5c45a905c53
maninbule <2373926690@qq.com>
2024-05-29 12:52:08 +0800

    添加了测试卷45

---
+++ 测试卷/测试卷45/01-463. 岛屿的周长.py
@@ -0,0 +1,7 @@
+'''
+https://leetcode.cn/problems/island-perimeter/description/
+'''
+
+class Solution:
+    def islandPerimeter(self, grid: list[list[int]]) -> int:
+        pass
---
+++ 测试卷/测试卷45/02-661. 图片平滑器.py
@@ -0,0 +1,8 @@
+'''
+https://leetcode.cn/problems/image-smoother/description/
+'''
+
+
+class Solution:
+    def imageSmoother(self, img: list[list[int]]) -> list[list[int]]:
+        pass
