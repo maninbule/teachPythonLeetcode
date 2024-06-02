d2c024ec0b2a160dbbd1c2ea73afe3188cdd5071
maninbule <2373926690@qq.com>
2024-06-02 15:22:54 +0800

    添加了测试卷46

---
+++ 测试卷/测试卷46/03-796. 旋转字符串.py
@@ -0,0 +1,7 @@
+'''
+https://leetcode.cn/problems/rotate-string/
+'''
+
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        pass
---
+++ 测试卷/测试卷46/04-1422. 分割字符串的最大得分.py
@@ -0,0 +1,7 @@
+'''
+https://leetcode.cn/problems/maximum-score-after-splitting-a-string/description/
+'''
+
+class Solution:
+    def maxScore(self, s: str) -> int:
+        pass
