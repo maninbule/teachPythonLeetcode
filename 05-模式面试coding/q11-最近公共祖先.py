f42a080765f9eb630097ab859371c089d1a7cd73
maninbule <2373926690@qq.com>
2024-06-12 21:08:06 +0800

    添加了模拟面试q10-12

---
+++ 05-模式面试coding/q11-最近公共祖先.py
@@ -0,0 +1,5 @@
+'''
+面试题 04.08. 首个共同祖先
+https://leetcode.cn/problems/first-common-ancestor-lcci/description/
+'''
+
---
+++ 05-模式面试coding/q12--前k小的数.py
@@ -0,0 +1,6 @@
+'''
+amazon 面试题
+面试题 17.14. 最小K个数
+https://leetcode.cn/problems/smallest-k-lcci/description/
+'''
+
