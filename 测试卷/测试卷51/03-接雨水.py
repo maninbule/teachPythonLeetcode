17ce70c2d31e84ff363702631b1442e94d2df4b6
maninbule <2373926690@qq.com>
2024-07-03 23:47:06 +0800

    给测试卷51添加了2个题目

---
+++ 测试卷/测试卷51/03-接雨水.py
@@ -0,0 +1,7 @@
+'''
+https://leetcode.cn/problems/trapping-rain-water/description/
+'''
+
+class Solution:
+    def trap(self, height: list[int]) -> int:
+        pass
---
+++ 测试卷/测试卷51/04-11. 盛最多水的容器.py
@@ -0,0 +1,7 @@
+'''
+https://leetcode.cn/problems/container-with-most-water/description/
+'''
+
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        pass
