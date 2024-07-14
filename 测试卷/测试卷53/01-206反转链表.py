6cf2bbbf736dc84a05284434d9d529c6bc78f8f1
maninbule <2373926690@qq.com>
2024-07-14 23:18:31 +0800

    提交了测试卷53

---
+++ 测试卷/测试卷53/01-206反转链表.py
@@ -0,0 +1,15 @@
+'''
+https://leetcode.cn/problems/reverse-linked-list/
+'''
+
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        pass
