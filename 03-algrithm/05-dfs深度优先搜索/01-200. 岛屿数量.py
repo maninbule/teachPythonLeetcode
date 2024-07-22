cc58e4b5c517a0bbdeef1132b931c351859c7099
maninbule <2373926690@qq.com>
2024-07-22 21:57:25 +0800

    添加了面试题

---
+++ 03-algrithm/05-dfs深度优先搜索/01-200. 岛屿数量.py
@@ -0,0 +1,8 @@
+'''
+https://leetcode.cn/problems/number-of-islands/description/
+'''
+
+
+class Solution:
+    def numIslands(self, grid: list[list[str]]) -> int:
+        pass
--- 05-模式面试coding/q23-289. 生命游戏.py
+++ 05-模式面试coding/q23-289. 生命游戏.py
@@ -3,9 +3,37 @@
 https://leetcode.cn/problems/game-of-life/description/
 '''
 
+
 class Solution:
     def gameOfLife(self, board: list[list[int]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        pass
+        if len(board) == 0:
+            return
+        n, m = len(board), len(board[0])
+        ans = [[0] * m for i in range(n)]
+        for i in range(n):
+            for j in range(m):
+                live = 0
+                for dx in [-1, 0, 1]:
+                    for dy in [-1, 0, 1]:
+                        if dx == 0 and dy == 0:
+                            continue
+                        x = i + dx
+                        y = j + dy
+                        if x < 0 or x >= n or y < 0 or y >= m:
+                            continue
+                        if board[x][y] == 1:
+                            live += 1
+                if board[i][j] == 1:
+                    if live < 2:
+                        ans[i][j] = 0
+                    elif live == 2 or live == 3:
+                        ans[i][j] = 1
+                    elif live > 3:
+                        ans[i][j] = 0
+                else:
+                    if live == 3:
+                        ans[i][j] = 1
+        board = ans
+        for i in range(n):
+            for j in range(m):
+                board[i][j] = ans[i][j]
