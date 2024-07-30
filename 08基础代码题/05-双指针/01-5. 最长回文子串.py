a09e69b5ac1a72c1f892439f3eb6f0d659306625
maninbule <2373926690@qq.com>
2024-07-30 11:15:17 +0800

    添加了基础代码题答案

--- 08基础代码题/05-双指针/01-5. 最长回文子串.py
+++ 08基础代码题/05-双指针/01-5. 最长回文子串.py
@@ -3,5 +3,21 @@ https://leetcode.cn/problems/longest-palindromic-substring/description/
 '''
 
 class Solution:
+    def getMaxlength(self,s:str,left:int,right:int)->str:
+        while left >=0 and right < len(s):
+            if s[left] != s[right]:
+                return s[left + 1:right] # [left+1,right-1]
+            left,right = left-1,right+1
+        return s[left+1:right] # [left+1,right-1]
     def longestPalindrome(self, s: str) -> str:
-        pass
+        ans = ""
+        for i in range(len(s)):
+            # i为中心轴.left = i-1,right = i+ 1
+            ans1 = self.getMaxlength(s,i-1,i+1)
+            if len(ans1) > len(ans):
+                ans = ans1
+            # i-1与i的中间空隙为中心轴，left=i-1,right = i
+            ans2 = self.getMaxlength(s,i-1,i)
+            if len(ans2) > len(ans):
+                ans = ans2
+        return ans
