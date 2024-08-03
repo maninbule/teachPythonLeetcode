73793c0bb0eccd4099ce99a41f7ce243c259c96e
maninbule <2373926690@qq.com>
2024-08-03 23:43:14 +0800

    添加了基础代码题

--- 08基础代码题/01-字符串/05-821. 字符的最短距离.py
+++ 08基础代码题/01-字符串/05-821. 字符的最短距离.py
@@ -3,7 +3,32 @@ https://leetcode.cn/problems/shortest-distance-to-a-character/description/
 '''
 from typing import List
 
-
+# O(n^2)
 class Solution:
     def shortestToChar(self, s: str, c: str) -> List[int]:
-        pass
+        n = len(s)
+        ans = [n] * n
+        for i in range(n):
+            for j in range(n):
+                if s[j] == c:
+                    ans[i] = min(ans[i],abs(i - j))
+        return ans
+
+# O(n)
+class Solution:
+    def shortestToChar(self, s: str, c: str) -> List[int]:
+        n = len(s)
+        ans = [n] * n
+        last = -1
+        for i in range(n):
+            if s[i] == c:
+                last = i
+            if last != -1:
+                ans[i] = min(ans[i], i - last)
+        last = -1
+        for i in range(n - 1, -1, -1):
+            if s[i] == c:
+                last = i
+            if last != -1:
+                ans[i] = min(ans[i], last - i)
+        return ans
--- 08基础代码题/03-哈希表/04-1189. “气球” 的最大数量.py
+++ 08基础代码题/03-哈希表/04-1189. “气球” 的最大数量.py
@@ -4,4 +4,16 @@ https://leetcode.cn/problems/maximum-number-of-balloons/description/
 
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        pass
+        from collections import defaultdict
+        table = defaultdict(int)
+        balloon = defaultdict(int)
+        for c in "balloon":
+            balloon[c] += 1
+        for c in text:
+            table[c] += 1
+        ans = len(text)
+        for c,count in balloon.items(): # a b l o n
+            cur = table[c] // count
+            ans = min(ans,cur)
+        return ans
+
