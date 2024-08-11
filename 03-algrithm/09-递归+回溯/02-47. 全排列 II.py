791d9990d61365f0cf79e56026a85e45d5c6fa05
maninbule <2373926690@qq.com>
2024-08-11 21:57:45 +0800

    添加了新算法

---
+++ 03-algrithm/09-递归+回溯/02-47. 全排列 II.py
@@ -0,0 +1,9 @@
+'''
+https://leetcode.cn/problems/permutations-ii/description/
+'''
+from typing import List
+
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        pass
--- 08基础代码题/03-哈希表/05-2367. 算术三元组的数目.py
+++ 08基础代码题/03-哈希表/05-2367. 算术三元组的数目.py
@@ -3,7 +3,31 @@ https://leetcode.cn/problems/number-of-arithmetic-triplets/
 '''
 from typing import List
 
+class Solution:
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        from collections import defaultdict
+        n = len(nums)
+        leftCnt = [0] * n # leftCnt[i] : 表示i左边有多少个nums[i] - diff这样的值
+        rightCnt = [0] * n # 同理 nums[i] + diff
+        count = defaultdict(int)
+        for i in range(n):
+            leftCnt[i] = count[nums[i] - diff]
+            count[nums[i]] += 1
+        count = defaultdict(int)
+        for i in range(n-1,-1,-1):
+            rightCnt[i] = count[nums[i] + diff]
+            count[nums[i]] += 1
+        ans = 0
+        for i in range(n): # 以i为三元组的中间下标，来计算能构造的三元组的个数
+            ans += leftCnt[i] * rightCnt[i]
+        return ans
 
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
-        pass
+        st = set()
+        ans = 0
+        for x in nums:
+            if x - diff in st and x - 2*diff in st:
+                ans += 1
+            st.add(x)
+        return ans
