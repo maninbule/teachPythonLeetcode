45b22c76882f4418f660ed48a499ece730f68f4f
maninbule <2373926690@qq.com>
2024-08-13 22:08:39 +0800

    添加了基础代码答案

--- 08基础代码题/06-数组/02-1365. 有多少小于当前数字的数字.py
+++ 08基础代码题/06-数组/02-1365. 有多少小于当前数字的数字.py
@@ -6,4 +6,29 @@ from typing import List
 
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        pass
+        n = len(nums)
+        nums = [[nums[i],i] for i in range(n)]
+        nums.sort(key = lambda x:x[0])
+        ans = []
+        j = -1
+        for i in range(n):
+            while j + 1 < n and nums[j + 1][0] < nums[i][0]:
+                j += 1
+            ans.append([j+1,nums[i][1]])
+        ans.sort(key = lambda x:x[1])
+        ans = [x[0] for x in ans]
+        return ans
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        nums = [[nums[i],i] for i in range(n)]
+        nums.sort(key = lambda x:x[0])
+        ans = [0] * n
+        j = -1
+        for i in range(n):
+            while j + 1 < n and nums[j + 1][0] < nums[i][0]:
+                j += 1
+            index = nums[i][1]
+            ans[index] = j + 1
+        return ans
--- 08基础代码题/11-dfs/01-39. 组合总和.py
+++ 08基础代码题/11-dfs/01-39. 组合总和.py
@@ -2,8 +2,25 @@
 https://leetcode.cn/problems/combination-sum/description/
 '''
 from typing import List
-
+import copy
 
 class Solution:
+    def dfs(self,i:int,choose:list[int],sum:int,candidates:list[int],target:int) -> None:
+        if sum == target:
+            self.ans.append(copy.copy(choose))
+            return
+        if sum > target or i == len(candidates):
+            return
+        # 选
+        choose.append(candidates[i])
+        self.dfs(i,choose,sum + candidates[i],candidates,target)
+        choose.pop()
+        # 不选
+        self.dfs(i + 1,choose,sum,candidates,target)
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        pass
+        self.ans = []
+        choose = []
+        self.dfs(0,choose,0,candidates,target)
+        return self.ans
+
+
