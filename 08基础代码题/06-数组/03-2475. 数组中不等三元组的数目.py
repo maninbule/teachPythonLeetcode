d13f6163687ba8fcc8de571a81521aac3174b1f4
maninbule <2373926690@qq.com>
2024-08-17 22:24:01 +0800

    添加了基础代码题

---
+++ 08基础代码题/06-数组/03-2475. 数组中不等三元组的数目.py
@@ -0,0 +1,9 @@
+'''
+https://leetcode.cn/problems/number-of-unequal-triplets-in-array/description/
+'''
+from typing import List
+
+
+class Solution:
+    def unequalTriplets(self, nums: List[int]) -> int:
+        pass
---
+++ 08基础代码题/11-dfs/02-77. 组合.py
@@ -0,0 +1,9 @@
+'''
+https://leetcode.cn/problems/combinations/description/
+'''
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        pass
