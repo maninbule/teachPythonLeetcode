9b7391c6e4ad12e1c4d27f868aa5d84b8c3e8cc2
maninbule <2373926690@qq.com>
2024-08-21 21:11:28 +0800

    添加了基础代码题

---
+++ 08基础代码题/11-dfs/03-695. 岛屿的最大面积.py
@@ -0,0 +1,9 @@
+'''
+https://leetcode.cn/problems/max-area-of-island/description/
+'''
+from typing import List
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        pass
