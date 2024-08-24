6464b99cd275f1247a3b64b2fe6411f0e6f51f1f
maninbule <2373926690@qq.com>
2024-08-24 22:29:24 +0800

    添加了算法原理

--- 08基础代码题/03-哈希表/07-1296. 划分数组为连续数字的集合.py
+++ 08基础代码题/03-哈希表/07-1296. 划分数组为连续数字的集合.py
@@ -6,4 +6,23 @@ from typing import List
 
 class Solution:
     def isPossibleDivide(self, nums: List[int], k: int) -> bool:
-        pass
+        nums.sort() # O(nlogn)
+        count = dict()
+        for x in nums:
+            if x in count:
+                count[x] += 1
+            else:
+                count[x] = 1
+        # O(n)
+        for x in nums:
+            if count[x] > 0:
+               need = count[x]
+               # x + 1, x + 2...x + k - 1都要有count[x]次
+               for y in range(x,x + k):
+                 if y not in count:
+                    return False
+                 if count[y] < need:
+                    return False
+                 else:
+                    count[y] -= need
+        return True
