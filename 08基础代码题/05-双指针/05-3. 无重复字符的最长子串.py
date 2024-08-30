0c40d9c1e8721861946d92a37f649662faefd95d
maninbule <2373926690@qq.com>
2024-08-30 23:12:02 +0800

    添

--- 08基础代码题/05-双指针/05-3. 无重复字符的最长子串.py
+++ 08基础代码题/05-双指针/05-3. 无重复字符的最长子串.py
@@ -4,4 +4,17 @@ https://leetcode.cn/problems/longest-substring-without-repeating-characters/desc
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        pass
+        n = len(s)
+        st = set()
+        j = 0
+        res = 0
+        for i in range(n):
+            while j < n and s[j] not in st: # 不重复就一直走
+                st.add(s[j])
+                j += 1
+            # j就处于一个重复的位置，或者已经越界了
+            # 以i为左端点，右端点j刚好走到此时的位置，就重复了，或者已经越界了
+            res = max(res, j - i)
+            # i要换成i + 1了，要从st中删除掉下标i的元素
+            st.remove(s[i])
+        return res
--- 08基础代码题/11-dfs/05-78. 子集.py
+++ 08基础代码题/11-dfs/05-78. 子集.py
@@ -1,9 +1,23 @@
 '''
 https://leetcode.cn/problems/subsets/
 '''
+import copy
 from typing import List
 
 
 class Solution:
+    def dfs(self,i:int,choose:list[int],nums:list[int],res:list[list]):
+        if i >= len(nums):
+            res.append(copy.copy(choose))
+            return
+        # 选择nums[i]
+        choose.append(nums[i])
+        self.dfs(i + 1,choose,nums,res)
+        choose.pop()
+        # 不选
+        self.dfs(i + 1,choose,nums,res)
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        pass
+        choose = []
+        res = []
+        self.dfs(0,choose,nums,res)
+        return res
