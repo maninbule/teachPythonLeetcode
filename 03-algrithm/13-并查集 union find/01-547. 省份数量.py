3b014c64bba30b343ae08dddb118793c37c0ea2c
maninbule <2373926690@qq.com>
2024-09-01 22:58:55 +0800

    111

--- 03-algrithm/13-并查集 union find/01-547. 省份数量.py
+++ 03-algrithm/13-并查集 union find/01-547. 省份数量.py
@@ -5,5 +5,32 @@ from typing import List
 
 
 class Solution:
+    # 第2步：写find函数，find函数是递归的
+    def find(self,x:int)->int:
+        if self.fa[x] != x:
+            self.fa[x] = self.find(self.fa[x])
+            return self.fa[x]
+        else:
+            return x
+    # 第3步：写union函数，表示要让x和y所在的集合进行合并，也就是x和y要在同一个集合
+    def union(self,x:int,y:int):
+        fx = self.find(x) # x所在集合的祖先
+        fy = self.find(y) # y所在集合的祖先
+        if fx != fy:   # 如果两个祖先不是同一个，就让其中一个祖先认另一个是自己的祖先
+            self.fa[fx] = fy
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        pass
+        n = len(isConnected)
+        # 第1步：初始化，让fa[i] = i ,各自为一个集合
+        self.fa = [0] * n
+        for i in range(n):
+            self.fa[i] = i
+        for i in range(n):
+            for j in range(i + 1,n): # i < j
+                if isConnected[i][j] == 1:
+                    self.union(i,j)
+        res = 0
+        for i in range(n):
+            if self.find(i) == i:
+                res += 1
+        return res
+
--- 03-algrithm/13-并查集 union find/02-1971. 寻找图中是否存在路径.py
+++ 03-algrithm/13-并查集 union find/02-1971. 寻找图中是否存在路径.py
@@ -3,7 +3,25 @@ https://leetcode.cn/problems/find-if-path-exists-in-graph/description/?envType=p
 '''
 from typing import List
 
-
 class Solution:
+    def find(self,x:int)->int:
+        if self.fa[x] != x:
+            self.fa[x] = self.find(self.fa[x])
+            return self.fa[x]
+        else:
+            return x
+    def union(self,x:int,y:int):
+        fx = self.find(x)
+        fy = self.find(y)
+        if fx != fy:
+            self.fa[fy] = fx
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        pass
+        self.fa = [0] * n
+        for i in range(n):
+            self.fa[i] = i
+        for x,y in edges:
+            self.union(x,y)
+        if self.find(source) == self.find(destination):
+            return True
+        else:
+            return False
