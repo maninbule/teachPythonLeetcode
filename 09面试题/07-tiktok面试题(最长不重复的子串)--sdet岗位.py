b10a3d041f9e3d08e0fb91473547c7a7c164d972
maninbule <2373926690@qq.com>
2024-09-19 18:58:15 +0800

    新加了模拟面试题

--- 09面试题/07-tiktok面试题(最长不重复的子串)--sdet岗位.py
+++ 09面试题/07-tiktok面试题(最长不重复的子串)--sdet岗位.py
@@ -2,3 +2,35 @@
 https://leetcode.cn/problems/longest-substring-without-repeating-characters/
 '''
 
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        st = set()
+        left = 0
+        res = 0
+        for right in range(len(s)):
+            while s[right] in st:
+                st.remove(s[left])
+                left += 1
+            st.add(s[right])
+            # 区间就是[left,right]这一段
+            res = max(res,right - left + 1)
+        return res
+
+# 输出不重复子串的具体字符串，而不是只是长度
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        st = set()
+        left = 0
+        res = 0
+        res_str = ""
+        for right in range(len(s)):
+            while s[right] in st:
+                st.remove(s[left])
+                left += 1
+            st.add(s[right])
+            # 区间就是[left,right]这一段
+            if right - left + 1 > res:
+                res = max(res,right - left + 1)
+                res_str = s[left:right + 1]
+        return res_str
--- 09面试题/08-tiktok面试题(199. 二叉树的右视图).py
+++ 09面试题/08-tiktok面试题(199. 二叉树的右视图).py
@@ -1,4 +1,25 @@
 '''
 https://leetcode.cn/problems/binary-tree-right-side-view/description/
 '''
+from typing import Optional, List
 
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def dfs(self,root,level):
+        if len(self.ans) < level:
+            self.ans.append(root.val)
+        if root.right:
+            self.dfs(root.right,level + 1)
+        if root.left:
+            self.dfs(root.left, level + 1)
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        self.ans = []
+        self.dfs(root,1)
+        return self.ans
