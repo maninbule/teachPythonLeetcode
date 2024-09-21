8954b5f495e989d4a77d6a99aa1f0923ad26e6ad
maninbule <2373926690@qq.com>
2024-09-21 20:39:57 +0800

    新加了google面试题答案

--- 09面试题/11-google面试题(断开所有叶子节点的最小代价).py
+++ 09面试题/11-google面试题(断开所有叶子节点的最小代价).py
@@ -1,11 +1,83 @@
 '''
-
 '''
 
 '''
 给你一个二叉树，树的边权大于0
 
 你可以删除你想删除的边，需要保证所有的叶子节点都与root节点没有连边。
-问删除的边的权值总和最小是多少
+问删除的边的权值总和最小是多少 ?
 
 '''
+
+class TreeNode:
+    def __init__(self,treenodeid:int):
+        self.treenodeid = treenodeid
+        self.left = None
+        self.right = None
+        self.leftWeight = 0
+        self.rightWeight = 0
+
+# 边权只等于等于0
+def dfs(root:TreeNode,WeightFromFatherNode:int)->int:
+    if root is None:
+        return 0
+    if root.left is None and root.right is None:
+        return WeightFromFatherNode
+    weightOfLeftSubTree = dfs(root.left,root.leftWeight)
+    weightOfRightSubTree = dfs(root.right, root.rightWeight)
+    return min(WeightFromFatherNode,weightOfLeftSubTree + weightOfRightSubTree)
+
+# 边权可以小于0，等于0，大于0
+def dfs(root:TreeNode,WeightFromFatherNode:int)->int:
+    if root is None:
+        return 0
+    if root.left is None and root.right is None:
+        return WeightFromFatherNode
+    weightOfLeftSubTree = dfs(root.left,root.leftWeight)
+    weightOfRightSubTree = dfs(root.right, root.rightWeight)
+    # 断开WeightFromFatherNode
+    cost1 =  WeightFromFatherNode + min(0,weightOfLeftSubTree) + min(0,weightOfRightSubTree)
+    # 不断开WeightFromFatherNode
+    cost2 = weightOfLeftSubTree + weightOfRightSubTree
+    return min(cost1,cost2)
+
+# if __name__ == '__main__':
+#     node = [TreeNode(i) for i in range(8)]
+#     node[1].left = node[2]
+#     node[1].right = node[3]
+#     node[2].left = node[4]
+#     node[2].right = node[5]
+#     node[3].left = node[6]
+#     node[3].right = node[7]
+#     node[1].leftWeight = 2
+#     node[1].rightWeight = 5
+#     node[2].leftWeight = 1
+#     node[2].rightWeight = 1
+#     node[3].leftWeight = 1
+#     node[3].rightWeight = 3
+#     inf = 2**30
+#     res = dfs(node[1],inf)
+#     print(res)
+
+if __name__ == '__main__':
+    node = [TreeNode(i) for i in range(8)]
+    node[1].left = node[2]
+    node[1].right = node[3]
+    node[2].left = node[4]
+    node[2].right = node[5]
+    node[3].left = node[6]
+    node[3].right = node[7]
+    node[1].leftWeight = -4
+    node[1].rightWeight = 5
+    node[2].leftWeight = -2
+    node[2].rightWeight = 5
+    node[3].leftWeight = 1
+    node[3].rightWeight = 3
+    inf = 2**30
+    res = dfs(node[1],inf)
+    print(res)
+
+# 刚才边的权值都是大于等于0的，现在要修改成边权可以是负数，0，正数
+# 现在要让总和最小
+
+
--- 09面试题/12-google面试题(子集).py
+++ 09面试题/12-google面试题(子集).py
@@ -1,4 +1,45 @@
 '''
 https://leetcode.cn/problems/subsets/
 '''
+import copy
+from typing import List
 
+
+class Solution:
+    def dfs(self,i:int,choose:list[int],nums:list[int]):
+        if i == len(nums):
+            self.res.append(copy.copy(choose))
+            return
+        # 对于第i个nums[i]：选与不选
+        # 选
+        choose.append(nums[i])
+        self.dfs(i + 1,choose,nums)
+        choose.pop()
+        # 不选
+        self.dfs(i + 1,choose,nums)
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        choose = []
+        self.dfs(0,choose,nums)
+        return self.res
+
+# 加一个限制，选择的子集不能超过x
+class Solution:
+    def dfs(self,i:int,choose:list[int],nums:list[int],x):
+        if sum(choose) > x:
+            return
+        if i == len(nums):
+            self.res.append(copy.copy(choose))
+            return
+        # 对于第i个nums[i]：选与不选
+        # 选
+        choose.append(nums[i])
+        self.dfs(i + 1,choose,nums,x)
+        choose.pop()
+        # 不选
+        self.dfs(i + 1,choose,nums,x)
+    def subsets(self, nums: List[int],x) -> List[List[int]]:
+        self.res = []
+        choose = []
+        self.dfs(0,choose,nums,x)
+        return self.res
