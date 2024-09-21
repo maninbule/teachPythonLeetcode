9a295e5b48cc76766f55ea20226a41a391365bbb
maninbule <2373926690@qq.com>
2024-09-21 19:22:02 +0800

    新加了google面试题

--- 09面试题/09-Amazon面试题(递增数组中最长连续数字的个数).py
+++ 09面试题/09-Amazon面试题(递增数组中最长连续数字的个数).py
@@ -1,6 +1,39 @@
 '''
 给你一个元素递增的数组:
 arr = [-1,0,1,1,1,2,2,3,4,4,4,4,4,7,7,7,8]
-
+求解相同数字连续的最大个数
 输出：5
 '''
+
+# 二分优化，可以优化这样的数据，有大量元素连续重复出现
+# 时间复杂度: O(klogn) k是不同元素的个数
+# 空间复杂度: O(1)
+def solution(arr:list[int])->int:
+    if arr == []:
+        return 0
+    res = 0
+    i = 0
+    while i < len(arr):
+        if i + 1 < len(arr) and arr[i + 1] != arr[i]:
+            res = max(res,1)
+            i = i + 1
+            continue
+        left,right = i,len(arr)-1
+        lastPost = -1
+        while left <= right:
+            mid = (left + right)//2
+            if arr[mid] == arr[i]:
+                lastPost = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        res = max(res,lastPost - i + 1)
+        i = lastPost + 1
+    return res
+
+print(solution([-1,0,1,1,1,2,2,3,4,4,4,4,4,7,7,7,8]))
+print(solution([-1]))
+print(solution([]))
+print(solution([1,1,1,1,1]))
+print(solution([1,2,5,6,7,8]))
+
--- 09面试题/10-Amazon面试题(双向链表转换成二叉搜索树).py
+++ 09面试题/10-Amazon面试题(双向链表转换成二叉搜索树).py
@@ -9,5 +9,77 @@
     8       20
    /  \     / \
   2   null null 24
+[2,8,16,20,24]
+输出就是这个BST的root节点
 '''
 
+class ListNode:
+    def __init__(self,val,pre = None,next = None):
+        self.val = val
+        self.pre = pre
+        self.next = next
+class TreeNode:
+    def __init__(self,val,left = None,right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    # [head,tail) [head,None)
+    def getMiddleListNode(self,head:ListNode,tail:ListNode):
+        length = 0
+        curNode = head
+        while curNode != tail:
+            length += 1
+            curNode = curNode.next
+        curNode = head
+        for i in range(length//2):
+            curNode = curNode.next
+        return curNode
+    def buildBST(self,head:ListNode)->TreeNode:
+        return self.buildBSTByDFS(head,None)
+    def buildBSTByDFS(self,head:ListNode,tail:ListNode):
+        # [head,tail)
+        if head == tail:
+            return None
+        # 节点个数大于等于1
+        midNode = self.getMiddleListNode(head,tail)
+        leftChild = self.getMiddleListNode(head,midNode)
+        rightChild = self.getMiddleListNode(midNode.next,tail)
+        midNode = TreeNode(midNode.val)
+        midNode.left = leftChild
+        midNode.right = rightChild
+        return midNode
+
+class Test:
+    def __init__(self):
+        self.inorderOfBST = []
+        self.orderOfList = []
+    def dfs(self,root:TreeNode):
+        if root == None:
+            return
+        if root.left:
+            self.dfs(root.left)
+        self.inorderOfBST.append(root.val)
+        if root.right:
+            self.dfs(root.right)
+    def getorderOfList(self,head:ListNode):
+        curNode = head
+        while curNode:
+            self.orderOfList.append(curNode.val)
+            curNode = curNode.next
+    def test(self,head:ListNode,root:TreeNode)->bool:
+        self.dfs(root)
+        self.getorderOfList(head)
+        return self.inorderOfBST == self.orderOfList
+
+
+if __name__ == '__main__':
+    # 2<->8<->16<->20<->24
+    val = [2,8,16,20,24]
+    arr = [ListNode(val[i]) for i in range(len(val))]
+    for i in range(len(val)-1):
+        arr[i].next = arr[i + 1]
+    head = arr[0]
+    root = Solution().buildBST(head)
+    print(Test().test(head,root))
