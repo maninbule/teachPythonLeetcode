b3ef7d2bfee9bf496db1ca373961f9e5385687d4
maninbule <2373926690@qq.com>
2024-10-08 20:03:04 +0800

    新加了测试题

---
+++ 08基础代码题/05-双指针/06-424. 替换后的最长重复字符.py
@@ -0,0 +1,7 @@
+'''
+https://leetcode.cn/problems/longest-repeating-character-replacement/description/?envType=problem-list-v2&envId=hash-table
+'''
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        pass
---
+++ 08基础代码题/14-排序/03-347. 前 K 个高频元素.py
@@ -0,0 +1,9 @@
+'''
+https://leetcode.cn/problems/top-k-frequent-elements/description/?envType=problem-list-v2&envId=hash-table
+'''
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        pass
